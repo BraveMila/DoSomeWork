7652ef8203eb04bddf2323cf43b5c5624e6d6483
Changmin Li <2567104993@qq.com>
2024-12-07 17:56:58 +0800

    first commit

---
+++ src/chapter01/chapter01_HelloWorld.java
@@ -0,0 +1,19 @@
+package chapter01;
+
+import java.awt.event.FocusAdapter;
+
+public class chapter01_HelloWorld {
+    /**
+     * 方法注释
+     * @param args
+     */
+    public static void main(String[] args) {
+        //单行注释
+        /*
+        多行注释
+        todo 待办事项颜色显示，在
+         */
+
+        System.out.println("Hello World");
+    }
+}
